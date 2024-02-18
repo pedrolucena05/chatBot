e8c1db6c4cadf424193adfb4bf5ed528d2cd10ba
pedrolucena05 <pedrolucena0523@gmail.com>
2024-02-18 02:28:15 -0300

    new version

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 
@@ -107,6 +108,7 @@ def hello_world():
     return 'chat botasasss mais outra uma vez por favor'
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
